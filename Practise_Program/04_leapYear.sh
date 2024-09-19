60cb064a3f1bb3bf1115f4fed2175c275ef4a0c1
sksatyam <killersksatyam@gmail.com>
2024-09-20 01:51:15 +0530

    04_leapYear.sh added

---
+++ Practise_Program/04_leapYear.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+read -p $'\nEnter a Year: ' year
+if((year%4 == 0))
+then 
+	if((year%100 == 0))
+	then
+		if((year%400 == 0))
+		then
+			echo "This is Leap Year!"
+			exit
+		else
+			echo "This is not a Leap Year!"
+			exit
+		fi
+	else
+		echo "This is a Leap Year!"
+		exit
+	fi
+else
+	echo "This is not a Leap Year!"
+	exit
+fi
+
