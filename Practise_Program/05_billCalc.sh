8f95f010f75dd66d3b7c3c9c89b8a6e274c5183f
sksatyam <killersksatyam@gmail.com>
2024-09-20 14:35:31 +0530

    sync

--- Practise_Program/05_billCalc.sh
+++ Practise_Program/05_billCalc.sh
@@ -1,8 +1,17 @@
 #/usr/bin/env bash
 read -p $'\nEnter Your power consumption in unit: ' unit
 sum=0
-if (( unit >1000 ))
+if (( unit >1000 ));
 then
-	$sum = $($sum+(($sum-1000)*8))
+	sum=$(($sum+($unit-1000)*8))
+	unit=1000
 fi
-echo $sum
+if((unit >500));
+then
+	sum=$(($sum+($unit-500)*7))
+	unit=500
+fi
+if ((unit>200));
+then
+	sum=$(($sum+($unit-200)*5.5))
+
